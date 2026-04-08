6e35f2475f59aca28499498461297a3c304eba98
gabrielvpina <gvpina.rodrigues@gmail.com>
2026-04-08 16:24:13 -0300

    initial commit

---
+++ viralquest/input_parser.py
@@ -0,0 +1,51 @@
+
+import os
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from loguru import logger
+
+# This script will handle all data input and parsing of fasta files.
+# I'm still learning some concepts about OOP, but my plan is aplly some of this concepts here.
+
+class FastaParser:
+
+    def __init__(self, file_path: str):
+
+        self.file_path: str = file_path
+        self.sequences: list[SeqRecord] = []
+        self.allowed_nucleotides: set[str] = set("ATCGUN-")
+
+        logger.info("Starting fasta parser")
+
+
+    def _is_nuc_only(self, record: SeqRecord) -> bool: # ensure that the sequences are composed by nucleotides
+
+        seq_text = str(record.seq).upper()
+        seq_chars = set(seq_text)
+
+        return seq_chars.issubset(self.allowed_nucleotides)
+
+
+    def read_input_file(self): # raw processing of input files
+
+        if not os.path.exists(self.file_path):
+            logger.error(f"O arquivo {self.file_path} não foi encontrado.")
+            raise ValueError("File not found.")
+
+        records_iterator = SeqIO.parse(handle=self.file_path, format="fasta")
+        
+        for record in records_iterator: # read each record and check if they're valid
+            if not self._is_nuc_only(record):
+                logger.error(f"The sequence {record.id} contains invalid characters.")
+                raise ValueError("The file contains sequences that are invalid to the analysis.")
+
+            self.sequences.append(record)
+
+        logger.success(f"Found {len(self.sequences)} valid sequences in {self.file_path}")
+ 
+
+
+meu_parser = FastaParser("meu_dna.fasta") 
+
+
+meu_parser.read_input_file()
